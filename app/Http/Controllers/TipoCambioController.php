22f6836e12a1876029fce840de827a0548c40e73
Aaron Tapia <atapiar88@gmail.com>
2025-04-11 22:49:41 -0500

    migration

---
+++ app/Http/Controllers/TipoCambioController.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTipoCambioRequest;
+use App\Http\Requests\UpdateTipoCambioRequest;
+use App\Models\TipoCambio;
+
+class TipoCambioController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreTipoCambioRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(TipoCambio $tipoCambio)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(TipoCambio $tipoCambio)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateTipoCambioRequest $request, TipoCambio $tipoCambio)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(TipoCambio $tipoCambio)
+    {
+        //
+    }
+}
